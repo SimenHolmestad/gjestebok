f58f3c8ba8427c7eb5bd2690564df94f89b76fde
Simen Holmestad <simen@holmestad.com>
2018-03-28 00:07:59 +0200

    Removed unnecessary code by changing language of server

--- Gjestebok/settings.py
+++ Gjestebok/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'nb-no'
 
 TIME_ZONE = 'Etc/GMT-1'
 
